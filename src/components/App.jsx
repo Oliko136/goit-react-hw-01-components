dd07b2c47dca0852a7cbaf5165a369a6c72c29d4
Oliko136 <olchick136@gmail.com>
2023-12-23 19:02:26 +0400

    Profile component created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import { Profile } from "./Profile/Profile";
+import user from "../data/user.json";
+
+const { username, tag, location, avatar, stats } = user;
+
+
 export const App = () => {
-  
+  return (
+    <Profile
+      username={username}
+      tag={tag}
+      location={location}
+      avatar={avatar}
+      stats={stats}
+    />
+  )
 };
