2f14a96ebec7b3367be60299cd61cabce79d077c
Oliko136 <olchick136@gmail.com>
2023-12-23 23:10:08 +0400

    Statistics component created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
 import user from "../data/user.json";
+import data from "../data/data.json";
 
 const { username, tag, location, avatar, stats } = user;
 
 
 export const App = () => {
   return (
-    <Profile
+    <>
+      <Profile
       username={username}
       tag={tag}
       location={location}
       avatar={avatar}
       stats={stats}
-    />
+      />
+      <Statistics title={"Upload stats"} stats={data}/>
+    </>
   )
 };
