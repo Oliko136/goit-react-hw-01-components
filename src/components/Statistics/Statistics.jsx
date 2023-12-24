d70ca739267d8265198abb25cd294aff4ca18d64
Oliko136 <olchick136@gmail.com>
2023-12-24 16:21:35 +0400

    TransactionHistory component created

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,10 +6,10 @@ export const Statistics = ({ title, stats }) => {
             {title && <h2 className="title">{title}</h2>}
 
             <ul className="stat-list">
-                {stats.map(stat => (
-                    <li key={stat.id} className="item">
-                        <span className="label">{stat.label}</span>
-                        <span className="percentage">{stat.percentage}</span>
+                {stats.map(({id, label, percentage}) => (
+                    <li key={id} className="item">
+                        <span className="label">{label}</span>
+                        <span className="percentage">{percentage}</span>
                     </li>
                 ))}
 
